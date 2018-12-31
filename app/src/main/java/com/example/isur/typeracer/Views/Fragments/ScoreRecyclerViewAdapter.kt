0bc045e5aa233caa65e4541ad5c1c662e3ee9496
Artur Bednarczyk <bednarczykartur96@gmail.com>
2018-12-31 01:58:52 +0100

    Created dialog for post score, update Score model, viewAdapter, created mockData, mockApi - post score.

--- app/src/main/java/com/example/isur/typeracer/Views/Fragments/ScoreRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/isur/typeracer/Views/Fragments/ScoreRecyclerViewAdapter.kt
@@ -35,7 +35,7 @@ class ScoreRecyclerViewAdapter(
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = mValues[position]
-        holder.mIdView.text = item.position.toString()
+        holder.mIdView.text = (position + 1).toString()
         holder.mContentView.text = item.nick
         holder.mScoreView.text = item.score.toString()
 
